0286047ef6cb07799b216cad5eea1486d095e975
Brian Oduory <brianmubweka17@gmail.com>
2025-03-25 18:36:15 +0300

    adds arrays

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+        cats.pop(name);
+    }
+    function destructivelyRemoveFirstCat(name) {
+        cats.shift(name);
+    }
+    function appendCat(name) {
+        return ["Milo", "Otis", "Garfield", name]
+    }
+    function prependCat(name) {
+        return [name, "Milo", "Otis", "Garfield"]
+    }
+    function removeLastCat(name) {
+        return ["Milo","Otis",]
+    }
+    function removeFirstCat(name) {
+        return ["Otis", "Garfield"]
+    }
